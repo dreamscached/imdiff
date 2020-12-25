f8bfbb5379d1ed4634855f3623ef8dbfd9d519c5
dreamscache.d <dreamscache.d@gmail.com>
2020-12-25 16:14:16 +0400

    Switch to ImageMagick++

--- src/imdiff.hpp
+++ src/imdiff.hpp
@@ -5,14 +5,11 @@
 #ifndef IMDIFF_IMDIFF_HPP
 #define IMDIFF_IMDIFF_HPP
 
-#include "opencv.hpp"
 #include "iostream.hpp"
 #include "string.hpp"
 
 namespace imdiff {
-    void equalSize(cv::Mat &first, cv::Mat &second);
 
-    double score(cv::Mat &first, cv::Mat &second);
 }
 
 #endif //IMDIFF_IMDIFF_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,9 @@
 
 #include "imdiff.hpp"
 
-using namespace cv;
 using namespace std;
 using namespace imdiff;
 
 int main(int argc, char **argv) {
-    if (argc < 3) {
-        cerr << "Usage: imdiff FIRST SECOND" << endl;
-        exit(0);
-    }
 
-    Mat first = imread(string(argv[1]));
-    Mat second = imread(string(argv[2]));
-
-    equalSize(first, second);
-    cout << score(first, second) << endl;
 }

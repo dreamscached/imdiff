49bb035bba6d82dd78c8b1cb93b76f7dac085cb9
dreamscache.d <dreamscache.d@gmail.com>
2020-12-27 14:12:08 +0400

    Add headers

---
+++ src/filesystem.hpp
@@ -0,0 +1,10 @@
+//
+// Created by dreamscached on 27.12.2020.
+//
+
+#ifndef IMDIFF_FILESYSTEM_HPP
+#define IMDIFF_FILESYSTEM_HPP
+
+#include <filesystem>
+
+#endif //IMDIFF_FILESYSTEM_HPP
---
+++ src/vector.hpp
@@ -0,0 +1,10 @@
+//
+// Created by dreamscached on 27.12.2020.
+//
+
+#ifndef IMDIFF_VECTOR_HPP
+#define IMDIFF_VECTOR_HPP
+
+#include <vector>
+
+#endif //IMDIFF_VECTOR_HPP

0bb22282cbdab1998e38faf275d667e375e59563
dreamscache.d <dreamscache.d@gmail.com>
2020-12-27 14:24:01 +0400

    Comments

--- src/fingerprint.cpp
+++ src/fingerprint.cpp
@@ -5,9 +5,11 @@
 #include "fingerprint.hpp"
 
 Magick::Image IMD::Fingerprint::getFingerprint(Magick::Image &image) {
+    // Turn image grayscale and shrink it to 8x8.
     Magick::Image fingerprint(image);
     fingerprint.type(Magick::GrayscaleType);
     fingerprint.resize(Magick::Geometry(8, 8));
+
     return fingerprint;
 }
 
@@ -19,15 +21,15 @@ double IMD::Fingerprint::getSimilarityScore(Magick::Image &first, Magick::Image 
     for (auto j = 0; j < 8; j++) {
         auto firstRow = firstPixels.getConst(0, j, 8, 1);
         auto secondRow = secondPixels.getConst(0, j, 8, 1);
+
         for (auto i = 0; i < 8; i++) {
+            // Since fingerprint image is grayscale, there's no need to fetch other channels.
+            // For instance, here we implicitly use R (pointer offset +0.)
             double firstValue = 255 * QuantumScale * *firstRow++;
             double secondValue = 255 * QuantumScale * *secondRow++;
 
-            if (IMD::Math::min(firstValue, secondValue) == firstValue) {
-                values[8 * j + i] = firstValue / secondValue;
-            } else {
-                values[8 * j + i] = secondValue / firstValue;
-            }
+            // We need to get double in range [0; 1], thus we need to divide lesser by greater.
+            values[8 * j + i] = IMD::Math::min(firstValue, secondValue) / IMD::Math::max(firstValue, secondValue);
         }
     }
 

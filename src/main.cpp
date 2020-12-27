f8ed2f3462d0705d719d2f4f3ba15430ce125e5f
dreamscache.d <dreamscache.d@gmail.com>
2020-12-27 14:11:59 +0400

    Rework

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,21 @@
 // Created by dreamscached on 25.12.2020.
 //
 
-#include "imdiff.hpp"
-
-using namespace std;
-using namespace imdiff;
+#include <CLI/CLI.hpp>
+#include "string.hpp"
+#include "vector.hpp"
 
 int main(int argc, char **argv) {
+    CLI::App app("image difference score calculator");
 
+    std::string originalPath;
+    app.add_option("originalPath", originalPath, "original image path");
+
+    std::vector<std::string> comparePaths;
+    app.add_option("comparePaths", comparePaths, "image paths to compare");
+
+    bool recurse;
+    app.add_option("-r,--recurse", recurse, "recurse into directories");
+
+    CLI11_PARSE(app, argc, argv)
 }

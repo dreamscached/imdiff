827918b2e7b8046139e35d6a2fc51b638058645a
dreamscache.d <dreamscache.d@gmail.com>
2020-12-29 18:54:10 +0400

    Add checks and mark as required

--- src/options.cpp
+++ src/options.cpp
@@ -7,13 +7,17 @@
 IMD::Options::Options IMD::Options::parseOptions(int argc, char **argv) {
     IMD::Options::Options options;
     CLI::App app("imdiff - image similarity score calculator");
-    app.add_option("ORIGINAL", options.originalPath, "original image path");
-    app.add_option("PATHS", options.processPaths, "image paths to process");
+    app.add_option("ORIGINAL", options.originalPath, "original image path")
+            ->check(CLI::ExistingFile)
+            ->required();
+    app.add_option("PATHS", options.processPaths, "image paths to process")
+            ->check(CLI::ExistingPath)
+            ->required();
     app.add_flag("-r,--recurse", options.recurse, "recurse into directories");
     app.add_flag("-p,--fingerprints", options.exportFingerprints,
                  "export fingerprints in Base64 format");
-    app.add_flag("-s,--score", options.printSimilarityScore,
-                 "print similarity score")->default_val(options.printSimilarityScore);
+    app.add_flag("-s,--score", options.printSimilarityScore, "print similarity score")
+            ->default_val(options.printSimilarityScore);
 
     try {
         app.parse(argc, argv);

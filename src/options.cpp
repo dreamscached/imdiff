23e6c20d419cdc04dace53cee360d70ff8758c91
dreamscache.d <dreamscache.d@gmail.com>
2020-12-29 22:17:13 +0400

    Capitalize help strings

--- src/options.cpp
+++ src/options.cpp
@@ -7,20 +7,20 @@
 IMD::Options::Options IMD::Options::parseOptions(int argc, char **argv) {
     IMD::Options::Options options;
     CLI::App app("imdiff - image similarity score calculator");
-    app.add_option("ORIGINAL", options.originalPath, "original image path")
+    app.add_option("ORIGINAL", options.originalPath, "Original image path")
             ->check(CLI::ExistingFile)
             ->required();
-    app.add_option("PATHS", options.processPaths, "image paths to process")
+    app.add_option("PATHS", options.processPaths, "Image paths to process")
             ->check(CLI::ExistingPath)
             ->required();
-    app.add_flag("-r,--recurse", options.recurse, "recurse into directories");
+    app.add_flag("-r,--recurse", options.recurse, "Recurse into directories");
     app.add_flag("-p,--fingerprints", options.exportFingerprints,
-                 "export fingerprints in Base64 format");
-    app.add_flag("-s,--score", options.printSimilarityScore, "print similarity score")
+                 "Export fingerprints in Base64 format");
+    app.add_flag("-s,--score", options.printSimilarityScore, "Print similarity score")
             ->default_val(options.printSimilarityScore);
-    app.add_flag("-n,--no-header", options.printHeader, "do not print table header")
+    app.add_flag("-n,--no-header", options.printHeader, "Do not print table header")
             ->default_val(options.printHeader);
-    app.add_flag("-H,--human-readable", options.humanReadable, "print score in human-readable format")
+    app.add_flag("-H,--human-readable", options.humanReadable, "Print score in human-readable format")
             ->default_val(options.humanReadable);
 
     app.callback([&] {
